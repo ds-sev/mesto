eb426e252b98ae156b566278e6b3788541a9026b
Dmitry Litvinenko <ds-sev@ya.ru>
2022-11-24 00:31:53 +0300

    add cards to DOM from js array

--- scripts/index.js
+++ scripts/index.js
@@ -11,34 +11,74 @@ const nameInput = document.querySelector('.profile-edit-form__field_get_name'); 
 const jobInput = document.querySelector('.profile-edit-form__field_get_job');                     //поле ввода занятия формы
 
 const likeButtonSwitch = () => {                                               //функция переключения активности кнопки Лайк
-    likeButton.forEach(like => {
-        like.addEventListener('click', (event) => {
-            event.target.classList.toggle('card__button-like_active');
-        })
+  likeButton.forEach(like => {
+    like.addEventListener('click', (event) => {
+      event.target.classList.toggle('card__button-like_active');
     })
+  })
 }
 
 const openPopup = function () {                                                //функция открытия попап
-    popup.classList.add('popup_opened');
-    nameInput.value = nameOnPage.textContent;
-    jobInput.value = jobOnPage.textContent;
+  popup.classList.add('popup_opened');
+  nameInput.value = nameOnPage.textContent;
+  jobInput.value = jobOnPage.textContent;
 }
 
 const closePopup = function () {                                               //функция закрытия попап
-    popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {                                               //функция получения данных из формы
-    evt.preventDefault();
-    nameOnPage.textContent = nameInput.value;
-    jobOnPage.textContent = jobInput.value;
-    closePopup();
+  evt.preventDefault();
+  nameOnPage.textContent = nameInput.value;
+  jobOnPage.textContent = jobInput.value;
+  closePopup();
 }
 
 likeButtonSwitch();                                                              //переключатель кнопки Лайк
 profileEditButton.addEventListener('click', openPopup);                     //открыть попап
 popupCloseButton.addEventListener('click', closePopup);                     //закрыть попап
 formElement.addEventListener('submit', formSubmitHandler);                  //отправка формы по нажатию кнопки Сохранить
+
+
+const initialCards = [
+  {
+    name: 'Севастополь',
+    link: 'https://images.unsplash.com/photo-1598177183267-28a7765536de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+  },
+  {
+    name: 'Коктебель',
+    link: 'https://images.unsplash.com/photo-1644745585878-2617e61fff00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+  },
+  {
+    name: 'Бахчисарай',
+    link: 'https://images.unsplash.com/photo-1621953231638-78df098abfb8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+  },
+  {
+    name: 'Алупка',
+    link: 'https://images.unsplash.com/photo-1601204057702-0bf266c98ca9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+  },
+  {
+    name: 'Белогорск',
+    link: 'https://images.unsplash.com/photo-1623527859001-8010a15cf790?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+  },
+  {
+    name: 'Ялта',
+    link: 'https://images.unsplash.com/photo-1624571149875-59a402116d88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+  }
+];
+
+
+const cardTemplate = document.querySelector('#card').content;
+const cardsSection = document.querySelector('.cards');
+for (let i = 0; i < initialCards.length; i++) {                                                      //Добавление в DOM карточек из массива
+  const cardName = initialCards[i].name;
+  const cardPhoto = initialCards[i].link;
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__photo-container').style.backgroundImage = `url(${cardPhoto})`;
+  cardElement.querySelector('.card__title').textContent = cardName;
+  cardsSection.append(cardElement);
+}
 
 
 
