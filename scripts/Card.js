0758e253fb95dab67fdaae13b5479284a046b353
Dmitry Litvinenko <ds-sev@ya.ru>
2022-12-19 23:57:49 +0300

    feature: создать класс Card

---
+++ scripts/Card.js
@@ -0,0 +1,45 @@
+/* CARD-CLASS */
+class Card {
+  constructor(cardData, handleImageClick) {
+    this._handleImageClick = handleImageClick;
+    this._name = cardData.name;
+    this._link = cardData.link;
+  }
+
+  _getTemplate() {
+    return document
+      .querySelector('#card')
+      .content
+      .querySelector('.card')
+      .cloneNode(true)
+  }
+
+  _handleTargetCardDelete(evt) {
+    evt.target.closest('.card').remove()
+  }
+
+  _setEventListeners() {
+    //переключатель лайков
+    const likeButton = this._element.querySelector('.card__button-like')
+    likeButton.addEventListener('click', () => {
+      likeButton.classList.toggle('card__button-like_active')
+    })
+    //слушатель для кнопки удаления карточки
+    const deleteButton = this._element.querySelector('#card-button-delete')
+    deleteButton.addEventListener('click', this._handleTargetCardDelete)
+    //слушатель для открытия полноэкранного изображения
+    this._element.querySelector('.card__photo-container').addEventListener('click', () =>
+      this._handleImageClick(this._link, this._name))
+  }
+  //возврат готовой к отрисовке карточки
+  generateCard() {
+    this._element = this._getTemplate()
+    this._element.querySelector('.card__title').textContent = this._name
+    this._element.querySelector('.card__photo-container').style.backgroundImage = `url(${this._link}`
+    this._setEventListeners()
+    return this._element
+  }
+}
+
+/* EXPORTS */
+export {Card}
--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@
 import {initialCards} from './cards.js';
 import {resetErrorMessages, startCheckForButton} from './validate.js';
 import {configValidation} from "./constants.js";
+import {Card} from "./Card.js";
 
 /* ПОЛЯ ПРОФИЛЯ НА СТРАНИЦЕ */
 const nameOnPage = document.querySelector('.profile__name');                                 //отображаемое на сайте имя
@@ -14,8 +15,7 @@ const profileEditButton = document.querySelector('.profile__button-edit'),      
   profilePopupButtonClose = document.querySelector('#profile-edit-form-button-close'),       //кнопка закрытия формы редактирования профиля
   profileEditForm = document.querySelector('#profile-edit-form');
 /* ДОБАВЛЕНИЕ МЕСТА */
-const cardTemplate = document.querySelector('#card').content.querySelector('.card'), //доступ к содержимому template для новой карточки
-  buttonNewCard = document.querySelector('.profile__button-add'),                             //кнопка добавления новой карточки
+const buttonNewCard = document.querySelector('.profile__button-add'),                             //кнопка добавления новой карточки
   popupNewCard = document.querySelector('#add-card-popup'),                                   //форма добавления нового места
   cardPopupButtonClose = document.querySelector('#add-card-form-button-close');               //кнопка закрытия формы добавления нового места
 /* ПРОСМОТР ИЗОБРАЖЕНИЯ */
@@ -80,55 +80,6 @@ function handleProfileEditFormSubmitData(evt) {
 
 profileEditForm.addEventListener('submit', handleProfileEditFormSubmitData);
 
-/* ФУНКЦИЯ ДОБАВЛЕНИЯ КАРТОЧКИ В РАЗМЕТКУ */
-const handleSubmitAddCardForm = (event) => {
-  event.preventDefault();
-  renderCard(cardsSection, {name: placeInput.value, link: linkInput.value});
-  closePopup(popupNewCard);
-}
-newCardForm.addEventListener('submit', handleSubmitAddCardForm);
-
-/* УДАЛЕНИЕ КАРТОЧКИ */
-const handleTargetCardDelete = (evt) => {
-  evt.target.closest('.card').remove()
-}
-
-/* ФУНКЦИЯ СОЗДАНИЯ КАРТОЧКИ */
-const generateCard = (cardData) => {
-  const cardElement = cardTemplate.cloneNode(true);
-  const cardName = cardElement.querySelector('.card__title');
-  const cardLink = cardElement.querySelector('.card__photo-container');
-  cardName.textContent = cardData.name;
-  cardLink.style.backgroundImage = `url(${cardData.link})`;
-  //функция переключения активности Лайков
-  const likeButton = cardElement.querySelector('#card__button-like');
-  likeButton.addEventListener('click', (event) => {
-    event.target.classList.toggle('card__button-like_active');
-  });
-  const deleteCardButton = cardElement.querySelector('#card-button-delete');
-  deleteCardButton.addEventListener('click', handleTargetCardDelete);
-  //просмотр полноэкранного изображения выбранной карточки
-  const imageCard = cardElement.querySelector('#image-card');
-  imageCard.addEventListener('click', () => {
-    imageViewItem.src = cardData.link;
-    imageViewTitle.textContent = cardName.textContent;
-    imageViewItem.alt = `На фото: ${cardName.textContent}`;
-    openPopup(imageViewPopup);
-  });
-  return cardElement;
-}
-
-/* ФУНКЦИЯ ДОБАВЛЕНИЯ КАРТОЧКИ В РАЗМЕТКУ */
-const renderCard = (section, cardData) => {
-  section.prepend(generateCard(cardData));
-}
-
-/* ПЕРЕБОР МАССИВА ПРЕДЗАГРУЖЕННЫХ КАРТОЧКЕК И ПЕРЕДАЧА КАЖДОГО ЭЛЕМЕНТА В ФУНКЦИЮ СОЗДАНИЯ НОВОЙ КАРТОЧКИ */
-initialCards.slice().reverse()
-  .forEach((initialCardData) => {
-    renderCard(cardsSection, initialCardData);
-  })
-
 /* ЗАКРЫТИЕ ФОРМЫ КЛИКОМ ПО ОВЕРЛЕЮ */
 popupList.forEach((overlay) => {
   overlay.addEventListener('click', (evt) => {
@@ -137,7 +88,6 @@ popupList.forEach((overlay) => {
     }
   })
 })
-
 /* ЗАКРЫТИЕ ФОРМЫ НАЖАТИЕМ ESC */
 const closePopupByEscKey = (evt) => {
   if (evt.code === 'Escape') {
@@ -145,5 +95,30 @@ const closePopupByEscKey = (evt) => {
     closePopup(popupOpen);
   }
 }
+/* ОТКРЫТИЕ ПОЛНОЭКРАННОГО ИЗОБРАЖЕНИЯ */
+const handleImageClick = (link, name) => {
+  imageViewItem.src = link;
+  imageViewTitle.textContent = name;
+  imageViewItem.alt = `На фото: ${name}`;
+  openPopup(imageViewPopup);
+}
 
+/* СОЗДАНИЕ НОВОЙ КАРТОЧКИ */
+const handleSubmitAddCardForm = (event) => {
+  event.preventDefault();
+  renderCard({name: placeInput.value, link: linkInput.value}, cardsSection);
+  closePopup(popupNewCard);
+}
+newCardForm.addEventListener('submit', handleSubmitAddCardForm);
+/* ДОБАВЛЕНИЕ КАРТОЧКИ В РАЗМЕТКУ */
+const renderCard = (cardData, section) => {
+  const card = new Card(cardData, handleImageClick)
+  section.prepend(card.generateCard());
+}
+/* ПЕРЕБОР МАССИВА ПРЕДЗАГРУЖЕННЫХ КАРТОЧКЕК И ОТРИСОВКА В РАЗМЕТКУ */
+initialCards.reverse().forEach((initialCardData) => {
+  renderCard(initialCardData, cardsSection)
+})
 
+/* EXPORTS */
+export {handleImageClick}
