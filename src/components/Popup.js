4d6f45ebdd546b9fa064bdfa15f8dbb072f6662c
Dmitry Litvinenko <ds-sev@ya.ru>
2023-01-23 11:32:37 +0300

    hotfix: edit _handleEscClose method in Popup class

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,18 +3,21 @@ export class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector)
   }
+
   open() {
     this._popup.classList.add('popup_opened');
+    this._handleEscClose = this._handleEscClose.bind(this)
     document.addEventListener('keydown', this._handleEscClose)
   }
+
   close() {
     this._popup.classList.remove('popup_opened')
     document.removeEventListener('keydown', this._handleEscClose)
   }
   //close popup by press Esc-button
   _handleEscClose(evt) {
-    if (evt.code === 'Escape' && document.querySelector('.popup_opened')) {
-      document.querySelector('.popup_opened').classList.remove('popup_opened');
+    if (evt.code === 'Escape') {
+      this.close()
     }
   }
   //listener for close popup by click on close icon or click on overlay
